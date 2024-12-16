5319cd56c5145f5f87baef114fe05c9f4c447f28
Vladimir <dyadichenko_va@mail.ru>
2024-12-16 15:31:31 +0300

    redaktor file js

--- js/js.js
+++ js/js.js
@@ -105,38 +105,3 @@ function initSlider() {
 }
 
 document.addEventListener("DOMContentLoaded", initSlider);
-
-
-
-// var controls = document.querySelectorAll('.controls');
-// for(var i=0; i<controls.length; i++){
-//   controls[i].style.display = 'inline-block';
-// }
-
-// var slides = document.querySelectorAll('#slides .slide');
-// var currentSlide = 0;
-// //var slideInterval = setInterval(nextSlide,2000); //Задает интервал в 2 секунды.
-
-// function nextSlide() {
-//     goToSlide(currentSlide+1);
-// };
-
-// function previousSlide() {
-//     goToSlide(currentSlide-1);
-// };
-
-// function goToSlide(n) {
-//     slides[currentSlide].className = 'slide';
-//     currentSlide = (n+slides.length)%slides.length;
-//     slides[currentSlide].className = 'slide showing';
-// };
-
-// var next = document.getElementById('next');
-// var previous = document.getElementById('previous');
-
-// next.onclick = function() {
-//     nextSlide();
-// };
-// previous.onclick = function() {
-//     previousSlide();
-// };
